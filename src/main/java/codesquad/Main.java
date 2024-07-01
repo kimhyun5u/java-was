a9660d2d87365f82ac5dcb0f16cd766b62e4b829
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-01 16:20:39 +0900

    refactor: System.out.println() 대신 slf4j의 구현체인 logback을 적용했다.

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,5 +1,8 @@
 package codesquad;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
@@ -9,11 +12,13 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
-        System.out.println("Listening for connection on port 8080 ....");
+
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Listening for connection on port 8080 ....");
 
         while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
             try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
-                System.out.println("Client connected");
+                logger.info("Client connected");
 
                 // HTTP 응답을 생성합니다.
                 OutputStream clientOutput = clientSocket.getOutputStream();
