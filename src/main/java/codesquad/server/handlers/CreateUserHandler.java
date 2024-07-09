0de9f4486b9987916d0c38e3ca11fa5e05605dd2
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-09 11:15:17 +0900

    fix: redirect 코드 수정

--- src/main/java/codesquad/server/handlers/CreateUserHandler.java
+++ src/main/java/codesquad/server/handlers/CreateUserHandler.java
@@ -12,7 +12,7 @@ public class CreateUserHandler {
     public static void createUser(Context ctx) {
         User user = new User(ctx.request().getQuery("userId"), ctx.request().getQuery("password"), ctx.request().getQuery("name"));
         var json = JsonConverter.toJson(user).getBytes();
-        ctx.response().setStatus(HttpStatus.REDIRECT_MOVE_PERMANENTLY)
+        ctx.response().setStatus(HttpStatus.REDIRECT_FOUND)
                 .addHeader("Location", "/")
                 .addHeader("Content-Type", "application/json")
                 .setBody(json);
