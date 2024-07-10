c05bf9c68447d9657713b7765bc1216974f9f85e
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-10 11:12:18 +0900

    refactor: 멀티 스레드 환경에 맞도록 수정 - 데이터베이스 별 락 설정 - 원자적 연산 적용 - 일관된 스냅샷 제공

--- src/main/java/codesquad/db/Database.java
+++ src/main/java/codesquad/db/Database.java
@@ -1,13 +1,16 @@
 package codesquad.db;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Database {
     private static final Map<String, Map<Object, Object>> db = new ConcurrentHashMap<>(); // 지워질 코드>
-
+    private static final Map<String, ReadWriteLock> locks = new ConcurrentHashMap<>();
     static {
         db.put("users", new ConcurrentHashMap<>());
         db.put("sessions", new ConcurrentHashMap<>());
@@ -17,11 +20,19 @@ public class Database {
     }
 
     public static void add(String dbname, Object key, Object value) {
-        db.get(dbname).putIfAbsent(key, value);
+        locks.computeIfAbsent(dbname, k -> new ReentrantReadWriteLock()).writeLock().lock();
+        try {
+            db.computeIfAbsent(dbname, k -> new ConcurrentHashMap<>()).put(key, value);
+        } finally {
+            locks.get(dbname).writeLock().unlock();
+        }
     }
 
     public static Optional<Object> get(String dbname, Object key) {
-        return Optional.ofNullable(db.get(dbname).get(key));
+        if (db.containsKey(dbname) && db.get(dbname).containsKey(key)) {
+            return Optional.ofNullable(db.get(dbname).get(key));
+        }
+        return Optional.empty();
     }
 
     public static void remove(String dbname, Object key) {
@@ -29,6 +40,6 @@ public class Database {
     }
 
     public static List<Object> getList(String dbname) {
-        return db.get(dbname).values().stream().toList();
+        return new ArrayList<>(db.getOrDefault(dbname, new ConcurrentHashMap<>()).values());
     }
 }
