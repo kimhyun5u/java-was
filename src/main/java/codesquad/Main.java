a5206df0d26516f40117258a4bd501410d7ca5c8
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-17 09:49:05 +0900

    refactor: Main 에 DependencyInjector 등록

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,5 +1,6 @@
 package codesquad;
 
+import codesquad.db.DependencyInjector;
 import codesquad.http.HttpStatus;
 import codesquad.http.Server;
 import codesquad.server.handlers.ArticleHandler;
@@ -15,17 +16,23 @@ public class Main {
 
 
     public static void main(String[] args) {
+        DependencyInjector.initialize();
+
+        UserHandler userHandler = DependencyInjector.getUserHandler();
+        ArticleHandler articleHandler = DependencyInjector.getArticleHandler();
+        ViewHandler viewHandler = DependencyInjector.getViewHandler();
+
         Server server = Server.defaultServer(PORT, THREAD_POOL_SIZE);
 
-        server.post("/user/create", UserHandler::createUser);
-        server.post("/user/login", UserHandler::login);
-        server.get("/register.html", ViewHandler::getRegistrationPage);
-        server.get("/", ViewHandler::getIndexPage);
-        server.post("/user/logout", UserHandler::logout);
-        server.get("/user/list", ViewHandler::getUserListPage);
-        server.get("/write", ViewHandler::getWritePage);
-        server.post("/write", ArticleHandler::write);
-        server.post("/comment", ArticleHandler::addComment);
+        server.post("/user/create", userHandler::createUser);
+        server.post("/user/login", userHandler::login);
+        server.get("/register.html", viewHandler::getRegistrationPage);
+        server.get("/", viewHandler::getIndexPage);
+        server.post("/user/logout", userHandler::logout);
+        server.get("/user/list", viewHandler::getUserListPage);
+        server.get("/write", viewHandler::getWritePage);
+        server.post("/write", articleHandler::write);
+        server.post("/comment", articleHandler::addComment);
 
         server.get("/user/login_failed", ctx ->
                 ctx.response()
