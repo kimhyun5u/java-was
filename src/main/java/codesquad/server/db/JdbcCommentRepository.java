d94919165a07f29c0dc7e24397ba75cc8edf3da5
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-19 07:59:02 +0900

    fix: AtomicLong 추가 - AUTO_INCREMENT 미구현으로 인한 추가

--- src/main/java/codesquad/server/db/JdbcCommentRepository.java
+++ src/main/java/codesquad/server/db/JdbcCommentRepository.java
@@ -7,22 +7,25 @@ import codesquad.model.User;
 import java.sql.Connection;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class JdbcCommentRepository implements CommentRepository {
     private static final String DBNAME = "comments";
     private final Connection conn;
+    private AtomicLong nextId = new AtomicLong(1);
 
     public JdbcCommentRepository(Connection connection) {
         this.conn = connection;
     }
 
     public void addComment(User user, Long pageId, String content) {
-        String sql = "INSERT INTO " + DBNAME + " (userId, pageId, username, content) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO " + DBNAME + " (id, userId, pageId, username, content) VALUES (?, ?, ?, ?, ?)";
         try (var pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, user.getUserId());
-            pstmt.setLong(2, pageId);
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, content);
+            pstmt.setString(1, nextId.getAndIncrement() + "");
+            pstmt.setString(2, user.getUserId());
+            pstmt.setLong(3, pageId);
+            pstmt.setString(4, user.getName());
+            pstmt.setString(5, content);
             pstmt.execute();
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -46,10 +49,7 @@ public class JdbcCommentRepository implements CommentRepository {
             pstmt.execute();
 
 
-            return DatabaseResolver.resultSetToList(pstmt.getResultSet()).stream()
-                    .filter(Objects::nonNull)
-                    .map(result -> new Comment(result.get("userId").toString(), Long.parseLong(result.get("pageId").toString()), result.get("username").toString(), result.get("content").toString()))
-                    .toList();
+            return DatabaseResolver.resultSetToList(pstmt.getResultSet()).stream().filter(Objects::nonNull).map(result -> new Comment(result.get("userId").toString(), Long.parseLong(result.get("pageId").toString()), result.get("username").toString(), result.get("content").toString())).toList();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
