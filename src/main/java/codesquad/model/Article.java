230ec36728bd668318023ce6b2ba291c6dc05548
kimhyun5u <22kimhyun5u@gmail.com>
2024-07-18 10:36:47 +0900

    feat: 변경된 article 정보 추가

--- src/main/java/codesquad/model/Article.java
+++ src/main/java/codesquad/model/Article.java
@@ -2,10 +2,12 @@ package codesquad.model;
 
 public class Article {
 
+    private Long id;
     private final String userId;
     private final String username;
-    private Long id;
     private final String content;
+    private String uploadPath;
+    private String originalName;
 
     public Article(String userId, String username, String content) {
         this.userId = userId;
@@ -13,11 +15,21 @@ public class Article {
         this.content = content;
     }
 
-    public Article(Long id, String userId, String username, String content) {
+    public Article(Long id, String userId, String username, String content, String uploadPath, String originalName) {
         this.userId = userId;
         this.username = username;
         this.content = content;
         this.id = id;
+        this.uploadPath = uploadPath;
+        this.originalName = originalName;
+    }
+
+    public String getUploadPath() {
+        return uploadPath;
+    }
+
+    public void setUploadPath(String uploadPath) {
+        this.uploadPath = uploadPath;
     }
 
     public long getId() {
@@ -38,6 +50,14 @@ public class Article {
 
     public String getContent() {
         return content;
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public void setOriginalName(String originalName) {
+        this.originalName = originalName;
     }
 }
 
